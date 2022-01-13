a44158c93a74dec2edea1f098ba52c6edddd827d
JustinMLin <linjus@iu.edu>
2022-01-13 15:10:23 -0500

    wip set_intersection

--- exercises/set_intersection/python/main.py
+++ exercises/set_intersection/python/main.py
@@ -1,15 +1,17 @@
+# Returns the intersection of two lists, tuples, or sets
 def intersection(list1,list2):
-    ret = []
-    list2_dict = {list2[i]:"" for i in range(len(list2))}
-    for element in list1:
-        if element in list2_dict and element not in ret:
-            ret.append(element)
-    return ret
+    try:
+        return set(list1) & set(list2)
+    except TypeError:
+        print("intersection: Inputs must be lists, tuples, or sets")
 
 def func_intersection(f1,f2,n):
-    list1 = [f1(i) for i in range(n+1)]
-    list2 = [f2(i) for i in range(n+1)]
-    return intersection(list1,list2)
+    try:
+        list1 = [f1(i) for i in range(n+1)]
+        list2 = [f2(i) for i in range(n+1)]
+        return intersection(list1,list2)
+    except TypeError:
+        print("func_intersection: Inputs must be functions defined on the integers")
 
 if __name__ == '__main__':
-    print(intersection([1,2,3,"a","b","c",1],[1,3,4,4,4,"a","a"]))
+    func_intersection(1,2,3)
