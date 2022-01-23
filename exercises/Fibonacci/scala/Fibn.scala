6e7d494a728b5194b6109f280df4b135d1471567
Nadav Kohen <nadavk25@gmail.com>
2022-01-23 15:10:56 -0500

    Cleaned up code

--- exercises/Fibonacci/scala/Fibn.scala
+++ exercises/Fibonacci/scala/Fibn.scala
@@ -1,18 +1,17 @@
 object Fibn {
-  def fib(x: Int) = {
-    var f1 = 1
-    var f2 = 1
+
+  def fib(input: Int): Int = {
+    var prev = 1
+    var next = 1
     var n = 1
-    var temp = 1
-    if (x<2) 1
-    else {
-      while (n<x) {
-        n += 1
-        temp = f1 + f2
-        f1 = f2
-        f2 = temp
-      }
-      temp
+    
+    while (n < input) {
+      n += 1
+      val temp = prev + next
+      prev = next
+      next = temp
     }
+
+    next
   }
 }
